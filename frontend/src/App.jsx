c20ba16422031b713e1fd6c0b63228a2af70b5aa
Md Ikbal <ikbal84336@gmail.com>
2025-08-11 22:56:55 +0530

    added pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
+const App = () => {
+  let authUser = null;
+  return (
+    <div className="flex flex-col items-center justify-start ">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={<LoginPage />} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
